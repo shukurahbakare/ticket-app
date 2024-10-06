333bb60e806a19b591cc7679399542a1c3e58ccd
Shukura Bakare <bshukuralillahi@cauridor.com>
2024-10-06 19:37:00 +0000

    Bottom Navbar

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,66 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+
+  final appScreens = [
+   const Center(child: Text("Home")),
+   const Center(child: Text("Search")),
+   const Center(child: Text("Tickets")),
+   const Center(child: Text("Profile")),
+  ];
+
+  //change index for bottom navbar
+  int _selectedIndex = 0;
+
+  void _onItemTapped (int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+    
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ticket Header'),
+        backgroundColor:const Color(0xFFFF9000),
+      ),
+      body: appScreens[_selectedIndex],
+      bottomNavigationBar:BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526400),
+        showSelectedLabels: false,
+        items:const[
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: 'Home'),
+
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: 'Search'),
+
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: 'Ticket'),
+
+              BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: 'Profile'),
+             
+        ]  
+      ),
+    );
+  }
+}
